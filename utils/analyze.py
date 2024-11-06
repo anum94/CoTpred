46c614b8979f7af5c55cf9ac84c8de1dbb6ed3df
Anum Afzal <anumafzaldogar@gmail.com>
2024-11-06 12:17:27 +0100

    ..

--- utils/analyze.py
+++ utils/analyze.py
@@ -1,15 +1,11 @@
 import pandas as pd
 
-df_no_cot = pd.read_excel("../runs/openai-gsm8k/processed_ds/openai-gsm8k_no_cot_train_test.xlsx")
+df_no_cot = pd.read_excel("../runs/processed_ds/gsm8k/CoT_False/openai-gsm8k_no_cot_train_test.xlsx")
 df_no_cot = df_no_cot.drop(['anum_decisions'], axis=1)
 df_no_cot.columns = ['Question', 'Reference', 'Prediction_no_cot','llm_decisions_no_cot']
 
-#df = pd.read_excel("../runs/openai-gsm8k/processed_ds/llama3_gsm8k_train_cot.xlsx")
-#df2 = pd.read_excel("../runs/openai-gsm8k/processed_ds/openai-gsm8k-test-cot.xlsx")
-#df3 = pd.concat([df,df2], axis = 0)
-#df3.to_excel("../runs/openai-gsm8k/processed_ds/openai-gsm8k_train_test_cot.xlsx")
 
-df_cot = pd.read_excel("../runs/openai-gsm8k/processed_ds/openai-gsm8k_train_test_cot.xlsx")
+df_cot = pd.read_excel("../runs/processed_ds/gsm8k/CoT_True/openai-gsm8k_train_test_cot.xlsx")
 df_cot = df_cot.drop(['anum_decisions', 'Question', 'Reference'], axis=1)
 df_cot.columns = [ 'Prediction_cot','llm_decisions_cot']
 
